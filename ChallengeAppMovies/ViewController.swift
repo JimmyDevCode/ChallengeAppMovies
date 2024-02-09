07db33c790db011099971a57a0e8d93039106f7f
JIMMY RONALDO MACEDO PIZANGO <jimmy.rmp@hotmail.com>
2024-02-08 23:51:40 -0500

    FEATURE: Se configura el repositorio subiendo los archivos iniciales

---
+++ ChallengeAppMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChallengeAppMovies
+//
+//  Created by Jimmy Ronaldo Macedo Pizango on 8/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
