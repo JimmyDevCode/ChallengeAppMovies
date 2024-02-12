ef6b33c4ba83fd9e094ecca5ae3384082932cdc9
JIMMY RONALDO MACEDO PIZANGO <jimmy.rmp@hotmail.com>
2024-02-12 00:37:30 -0500

    FEATURE: Se crea e implementa clase que mapea errores HttpClient a errores de Dominio

--- ChallengeAppMovies/Data/Repositoriy/MovieRepository.swift
+++ ChallengeAppMovies/Data/Repositoriy/MovieRepository.swift
@@ -17,26 +17,32 @@ protocol MovieRepositoryType {
 class MovieRepository: MovieRepositoryType{
     
     private let apiDataSource: APIMovieDataSource
+    private let errorMapper: MovieDomainErrorMapper
     
-    init(apiDataSource: APIMovieDataSource) {
+    init(apiDataSource: APIMovieDataSource, errorMapper: MovieDomainErrorMapper) {
         self.apiDataSource = apiDataSource
+        self.errorMapper = errorMapper
     }
     
     func getMovie() async -> Result<MovieResponseResult, MovieDomainError> {
         
         let movieListResult = await apiDataSource.getMovie()
         
+        
         guard case .success(let movieList) = movieListResult else {
             guard case .failure(let error) = movieListResult else {
                 return .failure(.generic)
             }
 
-            return .failure(error)
+            return .failure(errorMapper.map(error: error))
         }
 
         return .success(movieList)
     }
-    
-    
 }
 
+class MovieDomainErrorMapper{
+    func map(error: HTTPClientError?) -> MovieDomainError{
+        return .generic
+    }
+}
